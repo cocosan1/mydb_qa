b1ea4eb00b86e9c40dcf594f4d185511af1d3ac1
unknown <isishskw@gmail.com>
2024-01-25 09:56:24 +0900

    2nd commit

--- app.py
+++ app.py
@@ -2,18 +2,30 @@ import streamlit as st
 import os
 import requests
 # from notion_client import Client
-import datetime
+import os
+import shutil
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
 
 
 from llama_index import download_loader
 
-# pip install streamlit llama-index
+# pip install streamlit llama-index langchain langchain_community
 
 st.markdown('### 社内Q&A ')
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
 integration_token = st.secrets['NOTION_INTEGRATION_TOKEN']
+
+# embeddingモデルの初期化
+embedding_model = OpenAIEmbeddings() 
+
+# llmの初期化
+llm = ChatOpenAI(temperature=0) # temperature responseのランダム性0
 
 ##########################　notionからテキストデータの取得 関数内で使用
 def make_doc(notion_token):
@@ -59,11 +71,52 @@ def make_doc(notion_token):
 
     return documents
 
+raw_documents = make_doc(integration_token)
+
+st.write(raw_documents)
+
+### CharacterTextSplitter
+text_splitter = CharacterTextSplitter(
+    separator = "\n\n",  # セパレータ
+    chunk_size = 300,  # チャンクの文字数
+    chunk_overlap = 0,  # 重なりの最大文字数
+    length_function = len, # チャンクの長さがどのように計算されるか len 文字数 
+    is_separator_regex = False, 
+    # セパレータが正規表現かどうかを指定 True セパレータは正規表現 False 文字列
+)
+
+## データの分割
+# documentオブジェクト
+documents = text_splitter.split_documents(raw_documents)
+
+# テキストデータ　M25Retrieverに渡す前にオブジェクトから文字列に変換する必要あり。
+documents_txt = [doc.text for doc in text_splitter.split_documents(raw_documents)]
+
+
 #1次情報cbのindex化
 def save_fiass():
     ##############notionからテキストデータの取得
-    documents = make_doc(integration_token)
-    st.write(documents)
+
+    dir_path = './fiass_index/'
+
+    if os.path.isdir(dir_path):
+        # fiass_indexフォルダの削除
+        shutil.rmtree(dir_path)
+        st.write(f'{dir_path} 削除完了')
+
+        # vectorstoreの作成
+        vectorstore = FAISS.from_documents(documents, embedding_model)
+        # vectorstoreの保存
+        vectorstore.save_local("./fiass_index")
+        st.write('vectorstoreの保存完了')
+
+    else:
+        # vectorstoreの作成
+        vectorstore = FAISS.from_documents(documents, embedding_model)
+        # vectorstoreの保存
+        vectorstore.save_local("./fiass_index")
+        st.write('vectorstoreの保存完了')
+
 
 
 def main():
